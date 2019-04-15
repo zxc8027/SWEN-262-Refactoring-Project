782f37bb6895ea3c97eaaf7cdb0d3400d46bbc93
zxc8027 <zxc8027@g.rit.edu>
2019-04-15 13:41:19 -0400

    AddPartyView now uses command pattern

---
+++ src/UIEventHandler.java
@@ -0,0 +1,11 @@
+/**
+ * Interface for handling a user interface.
+ *
+ * @author Zachary Cook
+ */
+public interface UIEventHandler {
+    /**
+     * Handles the event.
+     */
+    void handleEvent();
+}
